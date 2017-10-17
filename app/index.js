a3b0ba98f48ce721a60c5a9888e0da94780dcb29
KaowNeaw <th.panya.bas@gmail.com>
2017-10-17 17:27:59 +0700

    add createClass for react15

--- app/index.js
+++ app/index.js
@@ -1,7 +1,8 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var createClass = require('create-react-class');
 
-var Hello = React.createClass({
+var Hello = createClass({
     render: function () {
 
         return (<div>Hello ReactJS Program!</div>);
