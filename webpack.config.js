68ea4b9037f8ad43663b010d20a18241b7297dce
KaowNeaw <th.panya.bas@gmail.com>
2017-10-17 17:23:28 +0700

    create component

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,10 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+    template: __dirname + '/app/index.html',
+    filename: 'index.html',
+    inject: 'body'
+});
+
 module.exports = {
     entry: [
         './app/index.js'
@@ -6,5 +13,10 @@ module.exports = {
         loaders: [
             {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
         ]
-    }
+    },
+    output: {
+        path: __dirname + '/dist',
+        filename: "index_bundle.js"
+    },
+    plugins: [HTMLWebpackPluginConfig]
 }
